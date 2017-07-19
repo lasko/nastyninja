6a056ab291ec88c3b131a91783cc4d5aaf503f04
Brandon Height <brandon.height@bodybuilding.com>
2017-07-19 14:08:46 -0700

    Adding edit/view handler tests

--- handlers_test.go
+++ handlers_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"net/http"
 	"net/http/httptest"
+	"os"
 	"testing"
 )
 
@@ -13,6 +14,50 @@ func TestIndexHandler(t *testing.T) {
 	}
 	rr := httptest.NewRecorder()
 	handler := http.HandlerFunc(indexHandler)
+
+	handler.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusOK)
+	}
+}
+
+func TestViewHandler(t *testing.T) {
+	p := newPage()
+	p.Title = "MockTest"
+	if err := p.save(); err != nil {
+		t.Fatal(err)
+	}
+	defer os.Remove(DataDirectory + p.Title + ".txt")
+	req, err := http.NewRequest("GET", "/view/TestPage", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	rr := httptest.NewRecorder()
+
+	handler := http.HandlerFunc(makeHandler(viewHandler))
+
+	handler.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusOK)
+	}
+}
+
+func TestEditHandler(t *testing.T) {
+	p := newPage()
+	p.Title = "MockTest"
+	if err := p.save(); err != nil {
+		t.Fatal(err)
+	}
+	defer os.Remove(DataDirectory + p.Title + ".txt")
+	req, err := http.NewRequest("GET", "/edit/MockTest", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	rr := httptest.NewRecorder()
+
+	handler := http.HandlerFunc(makeHandler(viewHandler))
 
 	handler.ServeHTTP(rr, req)
 
